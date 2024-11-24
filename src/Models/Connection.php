c2ff80a0ddc970bbec75bd0eb4c9dc3cf94829d6
Tomáš Vojík <vojik@wboy.cz>
2024-11-24 19:39:23 +0100

    :card_file_box: Connection model

---
+++ src/Models/Connection.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Lsr\Core\Models\Attributes\PrimaryKey;
+use Lsr\Core\Models\Model;
+use OpenApi\Attributes as OA;
+
+#[PrimaryKey('id_connection'), OA\Schema]
+class Connection extends Model
+{
+
+    public const string TABLE = 'connections';
+
+    #[OA\Property]
+    public bool $assigned = false;
+    #[OA\Property]
+    public bool $active = false;
+    #[OA\Property]
+    public int $speed = 1;
+    #[OA\Property]
+    public int $storageCapacity = 10;
+
+
+}
---
+++ src/Models/ConnectionStorage.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Lsr\Core\Models\Attributes\ManyToOne;
+use Lsr\Core\Models\Attributes\PrimaryKey;
+use Lsr\Core\Models\Model;
+use OpenApi\Attributes as OA;
+
+#[PrimaryKey('id_storage'), OA\Schema]
+class ConnectionStorage extends Model
+{
+    public const string TABLE = 'connection_storage';
+
+    #[ManyToOne, OA\Property]
+    public Connection $connection;
+    #[ManyToOne, OA\Property]
+    public Material $material;
+    #[OA\Property]
+    public int $quantity;
+}
